7e3d52332dc6cebec4a6218ff546e31481cc8211
Ray Perry <rperry@runoranj.com>
2019-03-12 20:25:30 -0500

    Adhoc | Delay the random message.

--- src/functions.ts
+++ src/functions.ts
@@ -26,6 +26,7 @@ const bank = require('./data/bank.json');
 let remainingMoons = STARTING_ECONOMY;
 let spareChangeCounter = SPARE_CHANGE_LIMIT;
 let spareChangeMessage: Message = null;
+let spareChangeTimeout: number = null;
 let lastSpareChangeClaim: number = null;
 
 const createEmbed = (embedColor: EmbedColor) => (senderName: string, message: string): RichEmbed => {
@@ -304,16 +305,19 @@ export const throwSpareChange = (message: Message) => {
     const currentChannelName = (message.channel as TextChannel).name;
     if (remainingMoons < SPARE_CHANGE_AMOUNT || !ALLOWED_CHANNELS.includes(currentChannelName) || spareChangeMessage) return;
     spareChangeCounter -= 1;
-    if (spareChangeCounter > 0) return;
+    if (spareChangeCounter > 0 || spareChangeTimeout != null) return;
 
     const response = createInfoEmbed('Free moons!', metaMessages.spareChange)
         .setImage('https://thumbs.gfycat.com/YawningPersonalEasteuropeanshepherd-max-1mb.gif');
 
-    message.channel.send(response)
-        .then(message => {
-            if (Array.isArray(message)) return;
-            spareChangeMessage = message;
-        });
+    clearTimeout(spareChangeTimeout);
+    spareChangeTimeout = setTimeout((message: Message) => {
+        message.channel.send(response)
+            .then(message => {
+                if (Array.isArray(message)) return;
+                spareChangeMessage = message;
+            });
+    }, 5000);
 }
 
 export const claimSpareChange = (message: Message) => {
